edc1fdd8e9cdbef9b5f8c0e977c5c1ea241b3726
Dmitriy Ovsienko <ovsienkodima@mail.ru>
2021-07-18 21:47:40 +0300

    close button view

--- src/components/photoblock/index.js
+++ src/components/photoblock/index.js
@@ -1,18 +1,63 @@
 import './style.css';
 import { photo } from '../constants';
+import { Component } from 'react';
 
-const PhotoBlock = () => {
-  return <div className="container">
+const Close = () => {
+    return <div className="close">
+                <div className="x"></div>
+                <div className="y"></div>
+            </div>
+}
+
+class PhotoBlock extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            photo: photo,
+            hover: false,
+            id: null,
+        };
+
+        this.mouseOver = this.mouseOver.bind(this);
+        this.mouseOut = this.mouseOut.bind(this);
+    }
+
+    mouseOver = (id) => {
+        this.setState({hover: true, id: id});
+    }
+    mouseOut = () => {
+        this.setState({hover: false});
+    }
+
+    render(){
+
+        const {hover, photo} = this.state;
+
+        return(
+            <div className="container">
             {
                 photo.map(item => {
                     return(
-                        <div className="photo" key={ item.id }>
-                            <img src={ item.src } alt=""></img>
+                        <div
+                        className="photo" 
+                        key={ item.id } 
+                        onMouseEnter={() => this.mouseOver(item.id)} 
+                        onMouseLeave={this.mouseOut}
+                        >
+                            {
+                                this.state.hover && this.state.id === item.id
+                                ? <Close></Close>
+                                : null
+                            }
+                            <img src={ item.src } alt={ item.name }></img>
                         </div>
                     )
                 })
             }
          </div>
+        );
+    };
 }
 
 export default PhotoBlock;

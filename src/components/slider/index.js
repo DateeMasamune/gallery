8d02e97ccb043422735cd760d3d232315443578b
Dmitriy Ovsienko <ovsienkodima@mail.ru>
2021-07-21 22:22:20 +0300

    one slide start

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -6,16 +6,38 @@ class Slider extends Component  {
 
     constructor(props){
         super(props)
-        this.state = {slide: photo}
+        this.state = {
+            slide: photo,
+            nexSlide: 0,
+            classNext: '',
+        }
+        this.nextSlide = this.nextSlide.bind(this);
+    }
+
+    nextSlide = () => {
+        const next = 1;
+        const classAdd = 'slider-img-prev';
+        this.setState({
+            classNext: classAdd,
+        })
+        console.log(this.state)
     }
 
     render(){
 
-        const {slide} = this.state;
-
+        const {slide, nexSlide, classNext} = this.state;
+        setTimeout(() => {
+            this.nextSlide();
+        }, 3000); 
         return(
             <div className="slider">
-                <img src={slide[0].src}></img>
+                <img
+                 className={classNext}
+                 src={slide[nexSlide].src}
+                 alt={slide[nexSlide].name}
+                 key={slide[nexSlide].id}>
+
+                 </img>
             </div>
         )
     }

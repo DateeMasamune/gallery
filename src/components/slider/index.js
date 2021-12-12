f3e8cc400a88ea8a446df97282c5cab7c1f8401d
Dmitriy Ovsienko <ovsienkodima@mail.ru>
2021-12-12 21:01:47 +0300

    last

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -10,107 +10,36 @@ class Slider extends Component  {
         super(props)
         this.state = {
             slide: photo,
-            currentSlide: 0,
-            slidePrev: "slider-img-prev",
-            slideNext: "slider-img-next",
-            centSlide: "",
-            counter: 0,
-            test: 0,
-            test1: 1,
-            test2: 2,
-            A: 0,
-            B: 1,
-            C: 2,
+            prev: "",
         }
-        this.nextSlide = this.nextSlide.bind(this);
-        this.nextPicture = this.nextPicture.bind(this);
+        this.coordsElement = this.coordsElement.bind(this);
+        this.setClass = this.setClass.bind(this)
     }
 
-    nextPicture = () => {
-        let a = this.state.A
-        let b = this.state.B
-        let c = this.state.C
-
-        a = c + 1;
-        b = c + 2;
-        c = c;
-
+    setClass = () => {
         this.setState({
-            A: a,
-            B: b,
-            C: c,
+            prev: "slider-img-prev",
         })
+        this.coordsElement();
     }
 
-    nextSlide = () => {
-
-        let counter = this.state.counter;
-        console.log(this.state.counter)
-        if(counter === 2){
-            
-            this.nextPicture()
-        }
-       
-        if(counter >= 2){
-            counter = 0;
-        
-            // this.test()
-            // this.nextImage();
-        }else{
-            
-            counter++
-        }
-
-       
-      
-       let centrS = "";
-       let nextS = "";
-       let prevS = "";
-
-       if(this.state.centSlide === ""){
-            centrS = "slider-img-prev";
-            nextS = "";
-            prevS = "speed";
-       }
-
-       if(this.state.centSlide === "slider-img-prev"){
-            centrS = "speed";
-            nextS = "slider-img-prev";
-            prevS = "";
-       }
-
-       if(this.state.centSlide === "speed"){
-            centrS = "";
-            nextS = "speed";
-            prevS = "slider-img-prev"; 
-       }
-
-
-       this.setState({
-           slidePrev: prevS,
-           slideNext: nextS,
-           centSlide: centrS,
-           counter: counter,
-        })
-       
-        
+    coordsElement = () => {
+        const obj = document.getElementsByClassName('slider-img-prev');
+        let posX =  obj.offsetLeft;
+        console.log(obj);
     }
 
-    componentDidMount = () =>{
-        
-        let interval = setInterval(this.nextSlide, 3000);
-        
+    componentDidMount = () => {
+        const interval = setInterval(this.setClass,3000)
     }
 
     render(){
         
-        const {slide, slidePrev, slideNext ,currentSlide, centSlide,test1,test2,test,A,B,C} = this.state;
+        const {slide,prev} = this.state;
         
         return(
             <div className="slider">
-              <img className={centSlide} src={slide[A].src}></img>
-              <img className={slideNext} src={slide[B].src}></img>
-              <img className={slidePrev} src={slide[C].src}></img>
+               <img className={prev} src={slide[0].src}></img>
             </div>
         )
     }
